b43f845523297d2ef48de17bf1dad504d7c75002
Jaclyn Barcal <cchaney@ChristohersMini.attlocal.net>
2023-10-18 15:07:20 -0500

    Adding procfile for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const PUBLIC_DIR = 'public';
 const DB_FILE = 'db/db.json';
 
